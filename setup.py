cc826ec40ab0233fce988c68279c7e235bb14cf1
Diego Garay-Ruiz <dgaray@iciq.es>
2021-07-07 15:55:15 +0200

    Improved setup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup,find_packages
-setup(name='AMK_RXReader',
-      version='0.02',
+setup(name='amk_tools',
+      version='0.5',
       author="Diego Garay-Ruiz",
       description="RxN management for AutoMeKin",
-      py_modules=['RXReader','RXVisualizer'])
+      py_modules=['RXReader','RXVisualizer'],
+      install_requires=['networkx','bokeh>=2.3.2','jsmol_bokeh_extension','matplotlib','numpy']
+      )
